a425409e1268d956c5fb93fc9a9a3288c2afa8c4
ETATUNG\Willy.Chen <Willy.Chen@etatung.com>
2026-03-18 14:27:03 +0800

    obj內容上傳

--- AngularApi/obj/Debug/net9.0/AngularApi.AssemblyInfo.cs
+++ AngularApi/obj/Debug/net9.0/AngularApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AngularApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cb6076711fb32108cbc11159e63171e1b12a671")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a624a330c36f67673636d3337cdaebffaaaced")]
 [assembly: System.Reflection.AssemblyProductAttribute("AngularApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AngularApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
